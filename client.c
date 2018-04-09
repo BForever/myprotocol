656f7cdf6873ecd5fce5ee93878d54e16cafd2c3
Bforever <11fhc@163.com>
2018-04-09 23:21:28 +0800

    v0.1

--- client.c
+++ client.c
@@ -1,62 +1,308 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include <pthread.h>
 
-#define SERVER_PORT 12345
+#define SERVER_PORT 2277
 #define BUF_SIZE 4096
 #define CMD_LENGTH 200
+#define CLIENT_NAME_SIZE 50
+
+enum msg_type
+{
+    UNKNOWN = 0, TIME, NAME, LIST, MESSAGE, REPLY, EXIT, DIS
+};
+typedef struct
+{
+    enum msg_type type;
+    int length;
+} Data;
+
+struct socketinfo
+{
+    int sa;
+    char name[CLIENT_NAME_SIZE];
+    char ip[16];
+    int port;
+};
 
 void fatal(char *string);
 
+void setblocking(int sa, int blocking);
+
+enum msg_type getcmd(char *);
+
+void *listen_msg(void *s);
+
+pthread_mutex_t msg_mutex = PTHREAD_MUTEX_INITIALIZER;
+int quit = 0;
+
 int main(int argc, char **argv)
 {
-    int c, s, bytes;
+    int c, s, bytes, i, n;
     char buf[BUF_SIZE];
+    char ip[16];
     struct hostent *h;
     struct sockaddr_in channel;
+    pthread_mutex_unlock(&msg_mutex);//init mutex
 
-    if (argc != 3)
-        fatal("Usage: client server-name file-name");
-
-    h = gethostbyname(argv[1]); /* look up host’s IP address */
-    if (!h)
-        fatal("gethostbyname failed");
-
-    s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (s < 0)
-        fatal("socket");
-
-    memset(&channel, 0, sizeof(channel));
-    channel.sin_family = AF_INET;
-    memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
-    channel.sin_port = htons(SERVER_PORT);
-    c = connect(s, (struct sockaddr *) &channel, sizeof(channel));
-    if (c < 0)
-        fatal("connect failed");
-
+    Data data;
     char cmd[CMD_LENGTH];
+
     while (1)
     {
-        fgets(cmd,CMD_LENGTH,stdin);
-        cmd[strlen(cmd)-1]=0;
+        printf("\n -------------------------------- \n");
+        printf("| Welcome to Client version 0.1! |\n");
+        printf("|--------------------------------|\n");
+        printf("| Type server ip to connect:     |\n");
+        printf("| (or just \"exit\")               |\n");
+        printf(" -------------------------------- \n");
+        printf("[client]");
+        fflush(stdout);
+        scanf("%s", ip);
+        if (!strcasecmp(ip, "exit"))
+        {
+            printf("[client] Exited. Welcome back!\n");
+            break;
+        }
 
-        printf("%s\n",cmd);
-        write(s, cmd, strlen(cmd) + 1);
+        h = gethostbyname(ip); /* look up host’s IP address */
+        if (!h)
+        {
+            fatal("[client]gethostbyname failed.");
+            continue;
+        }
+
+
+        s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (s < 0)
+        {
+            fatal("[client]create socket failed.");
+            continue;
+        }
+
+        memset(&channel, 0, sizeof(channel));
+        channel.sin_family = AF_INET;
+        memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
+        channel.sin_port = htons(SERVER_PORT);
+        c = connect(s, (struct sockaddr *) &channel, sizeof(channel));
+        if (c < 0)
+        {
+            fatal("[client]connect failed.");
+            continue;
+        }
+
+
+        // Send client name
+        FILE *p = popen("hostname", "r");
+//    read(p->_file, buf, CLIENT_NAME_SIZE);
+        fgets(buf, CLIENT_NAME_SIZE, p);
+        buf[CLIENT_NAME_SIZE - 1] = 0;
+        buf[strlen(buf) - 1] = 0;
+        write(s, buf, CLIENT_NAME_SIZE);
+        pclose(p);
+        getchar();
+        pthread_t listener;
+        quit = 0;
+        pthread_create(&listener, NULL, listen_msg, &s);
+
         while (1)
         {
-            bytes = read(s, buf, BUF_SIZE);
-            if (bytes <= 0)
-                break;
-            write(1, buf, bytes);
+            if(quit)break;
+            pthread_mutex_unlock(&msg_mutex);
+            printf("\n ------------------------------------------- \n");
+            printf("| Choose what you want to do:               |\n");
+            printf("| @ time: get server's time now             |\n");
+            printf("| @ name: get server's name                 |\n");
+            printf("| @ list: list all online clients           |\n");
+            printf("| @ msg : send message to certain client    |\n");
+            printf("| @ dis : disconnect from present server    |\n");
+            printf("| @ exit: exit the client                   |\n");
+            printf(" ------------------------------------------- \n");
+            printf("[client]");
+            fflush(stdout);
+            scanf("%s", cmd);
+
+            //printf("%s\n", cmd);
+
+            data.type = getcmd(cmd);
+
+            pthread_mutex_lock(&msg_mutex);
+            switch (data.type)
+            {
+                case TIME:
+                {
+//                printf("time cmd,send request\n");
+                    data.length = 0;
+                    write(s, &data, sizeof(Data));
+//                printf("req sent\n");
+                    read(s, &data, sizeof(Data));
+//                printf("response recved\n");
+                    read(s, buf, (size_t) data.length);
+//                printf("time recved\n");
+                    struct tm *tp = (struct tm *) buf;
+                    printf("[client] Server time: %02d:%02d:%02d %02d/%02d/%4d\n", tp->tm_hour, tp->tm_min, tp->tm_sec,
+                           tp->tm_mday,
+                           1 + tp->tm_mon, 1900 + tp->tm_year);
+                    break;
+                }
+                case NAME:
+                {
+//                printf("name cmd,send request\n");
+                    data.length = 0;
+                    write(s, &data, sizeof(Data));
+//                printf("req sent\n");
+
+                    read(s, &data, sizeof(Data));
+//                printf("response recved\n");
+                    read(s, buf, (size_t) data.length);
+                    buf[data.length - 1] = 0;
+//                printf("name recved\n");
+
+                    printf("[client] Server name: %s\n", buf);
+                    break;
+                }
+                case LIST:
+                {
+//                printf("list cmd,send request\n");
+                    data.length = 0;
+                    write(s, &data, sizeof(Data));
+//                printf("req sent\n");
+
+                    read(s, buf, sizeof(int));
+                    n = *(int *) buf;
+                    printf("[client] Connected clients: %d\n", n);
+                    printf("id\tname\n");
+                    for (i = 0; i < n; i++)
+                    {
+                        read(s, buf, sizeof(int));
+                        printf("%d\t", *(int *) buf);
+                        read(s, buf, CLIENT_NAME_SIZE);
+                        printf("%s\n", buf);
+                    }
+                    break;
+                }
+                case MESSAGE:
+                {
+                    int id;
+                    printf("[client] target client id:\n");
+                    scanf("%d", &id);
+                    printf("[client] Write the message you want to send and enter:\n");
+                    scanf("%s", cmd);
+
+                    data.length = strlen(cmd) + sizeof(int);
+                    *(int *) buf = id;
+                    memcpy(buf + sizeof(int), cmd, data.length);
+                    write(s, &data, sizeof(Data));
+                    write(s, buf, data.length + sizeof(int));
+//                printf("req sent\n");
+                    read(s, &data, sizeof(Data));
+                    if (data.length)
+                    {
+                        printf("[client] message successfully sent.\n");
+                    } else
+                    {
+                        printf("[client] target client not found.\n");
+                    }
+                    break;
+                }
+                case DIS:
+                {
+                    void *ret;
+                    quit = 1;
+                    pthread_join(listener, &ret);
+                    continue;
+                }
+                case EXIT:
+                {
+                    printf("[client] Exited. Welcome back!\n");
+                    exit(0);
+                }
+                default:
+                {
+                    break;
+                }
+            }
         }
     }
+
+
+}
+
+void *listen_msg(void *psocket)
+{
+    int s = *(int *) psocket;
+    int status;
+    char *buf = malloc(sizeof(BUF_SIZE));
+    Data data;
+    while (1)
+    {
+        pthread_mutex_lock(&msg_mutex);
+        setblocking(s, 0);
+        status = read(s, &data, sizeof(Data));
+        if (status == 0)
+        {
+            // Connection lost
+            setblocking(s, 1);
+            pthread_mutex_unlock(&msg_mutex);
+            break;
+        } else if (status > 0)
+        {
+            struct socketinfo info;
+            // Get msg
+            read(s, &info, sizeof(struct socketinfo));
+            read(s, buf, data.length);
+            buf[data.length] = 0;// For safety
+            printf("Received message from client %d: %s(%s:%d)\n", info.sa, info.name, info.ip, info.port);
+            printf("%s\n[client]", buf);
+            fflush(stdout);
+        }
+        setblocking(s, 1);
+        pthread_mutex_unlock(&msg_mutex);
+        usleep(5000);
+        if (quit)break;
+    }
+    free(buf);
+    close(s);
+    return NULL;
 }
 
 void fatal(char *string)
 {
     printf("%s\n", string);
+}
+
+enum msg_type getcmd(char *cmd)
+{
+    if (!strcasecmp(cmd, "time"))return TIME;
+    if (!strcasecmp(cmd, "name"))return NAME;
+    if (!strcasecmp(cmd, "list"))return LIST;
+    if (!strcasecmp(cmd, "msg"))return MESSAGE;
+    if (!strcasecmp(cmd, "dis"))return DIS;
+    if (!strcasecmp(cmd, "exit"))return EXIT;
+    return UNKNOWN;
+}
+
+void setblocking(int sa, int blocking)
+{
+    int flags = fcntl(sa, F_GETFL, 0);
+    if (flags == -1)
+    {
+        printf("[client] Errors while setting socket non-blocking!(get flag)\n");
+        return;
+    }
+    flags = (blocking ? flags & ~O_NONBLOCK : flags | O_NONBLOCK);
+    if (fcntl(sa, F_SETFL, flags) != 0)
+    {
+        printf("[client] Errors while setting socket non-blocking!(set flag)\n");
+        return;
+    }
+//    printf("Socket setting finished.\n");
 }
--- server.c
+++ server.c
@@ -1,22 +1,68 @@
 #include <sys/types.h>
-#include <sys/fcntl.h>
+#include <fcntl.h>
+#include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <arpa/inet.h>
 
-#define SERVER_PORT 12345
+
+#define SERVER_PORT 2277
 #define BUF_SIZE 4096
 #define QUEUE_SIZE 10
+#define CLIENT_NAME_SIZE 50
+
+enum msg_type
+{
+    UNKNOWN = 0, TIME, NAME, LIST, MESSAGE, REPLY
+};
+typedef struct
+{
+    enum msg_type type;
+    int length;
+} Data;
+
+typedef struct clientrecord *Client;
+struct socketinfo
+{
+    int sa;
+    char name[CLIENT_NAME_SIZE];
+    char ip[16];
+    int port;
+};
+struct clientrecord
+{
+    pthread_mutex_t *mutex;
+    pthread_t tid;
+    struct socketinfo info;
+    char *buf;
+    char *message;
+    int mesflag;
+    Client next;
+};
+
 
 void fatal(char *);
 
+void setblocking(int sa, int blocking);
+
+void *reply(void *arg);
+
+pthread_mutex_t head_mutex = PTHREAD_MUTEX_INITIALIZER;
+Client head = NULL;
+int headcnt = 0;
+
 int main(int argc, char *argv[])
 {
-    int s, b, l, fd, sa, bytes, on = 1;
+    int s, b, l, fd, bytes, on = 1;
     char buf[BUF_SIZE];
     struct sockaddr_in channel;
+    pthread_mutex_unlock(&head_mutex);// Init
+
 
     memset(&channel, 0, sizeof(channel));
     channel.sin_family = AF_INET;
@@ -24,39 +70,274 @@ int main(int argc, char *argv[])
     channel.sin_port = htons(SERVER_PORT);
 
     s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (s < 0) fatal("socket failed");
+    if (s < 0) fatal("[server] socket failed");
 
     setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on));
     b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
-    if (b < 0) fatal("bind failed");
+    if (b < 0) fatal("[server] bind failed");
 
     l = listen(s, QUEUE_SIZE); /* specify queue size */
-    if (l < 0) fatal("listen failed");
+    if (l < 0) fatal("[server] listen failed");
 
     while (1)
     {
-        sa = accept(s, 0, 0);
-        if (sa < 0) fatal("accept failed");
+//        printf("malloc client.\n");
+        Client newclient = malloc(sizeof(struct clientrecord));
 
-        read(sa, buf, BUF_SIZE);
-        printf("%s\n", buf);
+        newclient->info.sa = accept(s, 0, 0);
 
-        FILE *p = popen(buf, "r");
+        // Get ip address
+        struct sockaddr_in addr;
+        memset(&addr,0,sizeof(addr));
+        int len = sizeof(addr);
+        getsockname(newclient->info.sa,(struct sockaddr*)&addr,&len);
+        char* ip = inet_ntoa(addr.sin_addr);
+        memcpy(newclient->info.ip,inet_ntoa(addr.sin_addr),strlen(ip));
+        newclient->info.port=ntohs(addr.sin_port);
 
-        while (1)
+
+//        printf("new connection\n");
+        if (newclient->info.sa < 0)
         {
-            bytes = read(p->_file, buf, BUF_SIZE);
-            write(sa, buf, bytes);
-            write(1, buf, bytes);
-            if (bytes <= 0)
-                break;
+            fatal("[server] accept failed");
+            free(newclient);
+        } else
+        {
+            // Set new thread
+//            printf("set mutex\n");
+            newclient->mutex = malloc(sizeof(pthread_mutex_t));
+            pthread_mutex_init(newclient->mutex, NULL);
+//            printf("unlock mutex\n");
+            pthread_mutex_unlock(newclient->mutex);
+
+//            printf("set buf\n");
+            newclient->buf = malloc(BUF_SIZE);
+            newclient->message = malloc(BUF_SIZE);
+
+//            printf("read name\n");
+            read(newclient->info.sa, newclient->info.name, CLIENT_NAME_SIZE);
+
+            newclient->mesflag = 0;
+
+            // Add to clients list
+//            printf("add to list\n");
+            pthread_mutex_lock(&head_mutex);
+            newclient->next = head;
+            head = newclient;
+            headcnt++;
+            pthread_mutex_unlock(&head_mutex);
+            // Start new thread
+            printf("[server] new connection from %s, id %d assigned.\n",newclient->info.name,newclient->info.sa);
+            pthread_create(&newclient->tid, NULL, reply, (void *) newclient);
         }
-        pclose(p);
-        close(sa);
     }
+}
+
+void *reply(void *arg)
+{
+    Client this = arg;
+    int sa = this->info.sa;
+    char *buf = this->buf;
+    Data data;
+    int bytes;
+//    printf("new thread created.\n");
+
+
+    while (1)
+    {
+        //read command type
+        setblocking(sa, 0);
+        bytes = read(sa, &data, sizeof(Data));
+
+        if (bytes > 0)
+        {
+            setblocking(sa, 1);
+//            printf("request recved.\n");
+            switch (data.type)
+            {
+                case TIME:
+                {
+//                    printf("time req.\n");
+                    // Get time
+                    time_t t;
+                    time(&t);
+                    struct tm *tp;
+                    tp = localtime(&t);
+//                    printf("Server time: %02d:%02d:%02d %02d/%02d/%4d\n", tp->tm_hour, tp->tm_min, tp->tm_sec,
+//                    tp->tm_mday,
+//                            1 + tp->tm_mon, 1900 + tp->tm_year);
+                    // Get struct size
+                    bytes = sizeof(struct tm);
+
+                    // Encapsulate
+                    memcpy(buf, tp, bytes);
+                    data.length = bytes;
+
+                    // Send
+                    write(sa, &data, sizeof(Data));
+                    write(sa, buf, bytes);
+//                    printf("time sent.\n");
+                    break;
+                }
+
+                case NAME:
+                {
+                    int i;
+//                    printf("name req.\n");
+                    // Get name
+                    FILE *p = popen("hostname", "r");
+
+                    // Get struct size
+                    bytes = read(p->_file, buf, BUF_SIZE);
+
+                    (*(char *) (buf + CLIENT_NAME_SIZE - 1)) = 0;
+                    (*(char *) (buf + strlen(buf) - 1)) = 0;
+                    data.length = bytes;
+                    write(sa, &data, sizeof(Data));
+                    write(sa, buf, bytes);
+                    pclose(p);
+
+//                    printf("name sent.\n");
+                    break;
+                }
+                case LIST:
+                {
+                    Client tmp;
+//                    printf("list req.\n");
+
+                    pthread_mutex_lock(&head_mutex);
+                    tmp = head;
+                    write(sa, &headcnt, sizeof(int));
+                    while (tmp)
+                    {
+                        write(sa, &tmp->info.sa, sizeof(int));
+                        write(sa, tmp->info.name, CLIENT_NAME_SIZE);
+                        tmp = tmp->next;
+                    }
+                    pthread_mutex_unlock(&head_mutex);
+//                    printf("list sent.\n");
+                    break;
+                }
+                case MESSAGE:
+                {
+                    data.type=REPLY;
+                    read(sa, buf, data.length);
+                    int target = *(int *) buf;
+                    char *text = buf + sizeof(int);
+                    // Find target client
+                    Client tmp;
+                    pthread_mutex_lock(&head_mutex);
+                    tmp = head;
+                    while (tmp && tmp->info.sa != target)tmp = tmp->next;
+                    pthread_mutex_unlock(&head_mutex);
+                    if (tmp)
+                    {
+                        pthread_mutex_lock(tmp->mutex);
+                        // Wait for the former message to be sent
+                        while (tmp->mesflag)
+                        {
+                            pthread_mutex_unlock(tmp->mutex);
+                            usleep(5000);
+                            pthread_mutex_lock(tmp->mutex);
+                        }
+                        memcpy(tmp->message, text, data.length);
+                        memcpy(tmp->buf,&this->info, sizeof(struct socketinfo));
+                        tmp->mesflag = 1;
+                        pthread_mutex_unlock(tmp->mutex);
+
+                        data.length=1;
+                        write(sa,&data, sizeof(Data));
+                    } else
+                    {
+                        // Client not found, reply negative
+                        data.length=0;
+                        write(sa,&data, sizeof(Data));
+                    }
+                    break;
+                }
+                default:
+                    break;
+
+            }
+        } else if (!bytes)
+        {
+            // Connection lost
+            printf("[server] %d:%s lost connection.\n", this->info.sa,this->info.name);
+            pthread_mutex_lock(&head_mutex);
+            Client tmp = head;
+            Client prev;
+            while (tmp != this && tmp)
+            {
+                prev = tmp;
+                tmp = tmp->next;
+            }
+            if (tmp)
+            {
+                if (head == tmp)
+                {
+                    head = head->next;
+                } else
+                {
+                    prev->next = tmp->next;
+                }
+                pthread_mutex_destroy(tmp->mutex);
+                free(tmp->buf);
+                free(tmp->message);
+                close(tmp->info.sa);
+                free(tmp);
+                headcnt--;
+            } else
+            {
+                fatal("[server] Thread not found!!!");
+                while (1);
+            }
+            pthread_mutex_unlock(&head_mutex);
+            break;
+
+        } else
+        {
+            // Check message
+            // printf("no bytes received.\n");
+            if (this->mesflag)
+            {
+                // Get message from client
+                pthread_mutex_lock(this->mutex);
+                Data data;
+                data.type = MESSAGE;
+                data.length = strlen(this->message);
+                write(sa, &data, sizeof(Data));
+                write(sa,this->buf, sizeof(struct socketinfo));
+                write(sa, this->message, data.length);
+                this->mesflag = 0;
+                pthread_mutex_unlock(this->mutex);
+            }
+            usleep(5000);
+
+        }
+
+
+    }
+    return NULL;
 }
 
 void fatal(char *string)
 {
     printf("%s\n", string);
+}
+
+void setblocking(int sa, int blocking)
+{
+    int flags = fcntl(sa, F_GETFL, 0);
+    if (flags == -1)
+    {
+        printf("[server] Errors while setting socket non-blocking!(get flag)\n");
+        return;
+    }
+    flags = (blocking ? flags & ~O_NONBLOCK : flags | O_NONBLOCK);
+    if (fcntl(sa, F_SETFL, flags) != 0)
+    {
+        printf("[server] Errors while setting socket non-blocking!(set flag)\n");
+        return;
+    }
+//    printf("Socket setting finished.\n");
 }
